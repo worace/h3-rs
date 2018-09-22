7d3d9e053bb4dc8f337c4a76aa22f8908f7f56aa
Horace Williams <horace.d.williams@gmail.com>
2018-09-22 13:07:10 -0700

    Add geo_to_face test cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 extern crate geo_types;
+use std::fmt;
 
 // use geo_types::Coordinate;
 
@@ -32,6 +33,12 @@ struct GeoCoord {
     lon: f64
 }
 
+impl fmt::Display for GeoCoord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "GeoCoord(lat: {}, lon: {})", self.lat, self.lon)
+    }
+}
+
 impl GeoCoord {
     fn new(lat: f64, lon: f64) -> GeoCoord {
         GeoCoord{lat: lat, lon: lon}
@@ -60,7 +67,7 @@ fn square_distance_3d(a: &Vec3d, b: &Vec3d) -> f64 {
     square(a.x - b.x) + square(a.y - b.y) + square(a.z - b.z)
 }
 
-fn geo_to_face(geo: GeoCoord) -> usize {
+fn geo_to_face(geo: &GeoCoord) -> usize {
     let v3d = geo_to_coord_3d(geo);
     let mut face = 0;
     let mut min_dist = square_distance_3d(&v3d, &constants::FACE_CENTERS[0]);
@@ -74,7 +81,7 @@ fn geo_to_face(geo: GeoCoord) -> usize {
     face
 }
 
-fn geo_to_coord_3d(geo: GeoCoord) -> Vec3d {
+fn geo_to_coord_3d(geo: &GeoCoord) -> Vec3d {
     let r = geo.lat.cos();
 
     Vec3d::new(
@@ -83,24 +90,16 @@ fn geo_to_coord_3d(geo: GeoCoord) -> Vec3d {
         geo.lat.sin()
     )
 }
-// 27: ~~~ Get 3d (x,y,z) coords for lat/lon in rads
-// 27: ~~~ (x,y,z) for rads (-0.094485, 1.552838)
-// 27: ~~~ (0.017878, 0.995379, -0.094345)
-// 27: ~~~ Coord: (-0.056663, 1.634397)~~~ 3 - _geoToVec3d
-// 27: ~~~ Get 3d (x,y,z) coords for lat/lon in rads
-// 27: ~~~ (x,y,z) for rads (-0.056663, 1.634397)
-// 27: ~~~ (-0.063456, 0.996376, -0.056633)
-// 27: ~~~ Coord: (-0.219365, 1.115781)~~~ 3 - _geoToVec3d
-// 27: ~~~ Get 3d (x,y,z) coords for lat/lon in rads
-// 27: ~~~ (x,y,z) for rads (-0.219365, 1.115781)
-// 27: ~~~ (0.428944, 0.876729, -0.217610)
 
 #[cfg(test)]
 mod tests {
     use geo_to_coord_3d;
+    use geo_to_face;
     use square_distance_3d;
     use Vec3d;
     use GeoCoord;
+    use std::fs::File;
+    use std::io::prelude::*;
 
     fn assert_eq_floats(a: f64, b: f64) -> () {
         let epsilon = 0.000001;
@@ -125,12 +124,36 @@ mod tests {
              Vec3d::new(0.428944, 0.876729, -0.217610))
         ];
         for (coord, v3d) in pairs {
-            assert_eq_v3d(v3d, geo_to_coord_3d(coord));
+            assert_eq_v3d(v3d, geo_to_coord_3d(&coord));
         }
+    }
+
+    fn geo_to_face_test_cases() -> Vec<(GeoCoord, usize)> {
+        let mut f = File::open("./tests/resources/geo_coord_face_test_cases.tsv").expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).expect("Failed to read test file");
+        let lines = contents.split("\n");
+        let tsv = lines.filter(|l| !l.is_empty()).map(|l| l.split("\t"));
+        tsv.map(|split_cols| {
+            let cols: Vec<&str> = split_cols.collect();
+            let lat_rads: f64 = cols[0].parse().unwrap();
+            let lon_rads: f64 = cols[1].parse().unwrap();
+            let face: usize = cols[2].parse().unwrap();
+            (GeoCoord::new(lat_rads, lon_rads), face)
+        }).collect()
     }
 
     #[test]
     fn test_geo_to_face() {
+        for pair in geo_to_face_test_cases() {
+            assert_eq!(
+                geo_to_face(&pair.0),
+                pair.1,
+                "Expected GeoCoord {} to be nearest face {}",
+                pair.0,
+                pair.1
+            );
+        }
     }
 
     #[test]
